209b8c8548816b9ee61c10821149828b1095c036
raeyoung <raeyoung233@gmail.com>
2020-11-11 23:42:19 +0900

    Create onToggle function

--- src/App.js
+++ src/App.js
@@ -48,10 +48,22 @@ const App = () => {
     [todos],
   );
 
+  // 할일 수정
+  const onToggle = useCallback(
+    (id) => {
+      setTodos(
+        todos.map((todo) =>
+          todo.id === id ? { ...todo, checked: !todo.checked } : todo,
+        ),
+      );
+    },
+    [todos],
+  );
+
   return (
     <TodoTemplate>
       <TodoInsert onInsert={onInsert} />
-      <TodoList todos={todos} onRemove={onRemove} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
   );
 };
