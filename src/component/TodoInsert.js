effa4c88ba7e072773b0ba553e55e5d8425af336
raeyoung <raeyoung233@gmail.com>
2020-11-11 23:28:33 +0900

    Create onSubmit function at the TodoInsert.js

--- src/component/TodoInsert.js
+++ src/component/TodoInsert.js
@@ -2,14 +2,27 @@ import React, { useState, useCallback } from 'react';
 import { MdCheckCircle } from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
   const [value, setValue] = useState('');
 
   const onChange = useCallback((e) => {
     setValue(e.target.value);
   }, []);
+
+  const onSubmit = useCallback(
+    (e) => {
+      onInsert(value);
+      setValue(''); // value 초기화
+
+      // submit Event 는 브라우저에서 새로고침할 경우 발생
+      // 방지하기 위해 함수 호출
+      e.preventDefault();
+    },
+    [onInsert, value],
+  );
+
   return (
-    <form className="TodoInsert">
+    <form className="TodoInsert" onSubmit={onSubmit}>
       <input
         placeholder="할 일을 입력하세요"
         value={value}
