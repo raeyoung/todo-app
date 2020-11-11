69180ef4b9d165fe618039b2827d19bc5f000c19
raeyoung <raeyoung233@gmail.com>
2020-11-11 23:22:26 +0900

    todo  array 에 새 객체 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import TodoTemplate from './component/TodoTemplate';
 import TodoInsert from './component/TodoInsert';
 import TodoList from './component/TodoList';
@@ -22,9 +22,26 @@ const App = () => {
     },
   ]);
 
+  // 고유값으로 사용될 id
+  // ref 를 사용하여 변수에 담기
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(
+    (text) => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1; // nextId 에 +1
+    },
+    [todos],
+  );
+
   return (
     <TodoTemplate>
-      <TodoInsert />
+      <TodoInsert onInsert={onInsert} />
       <TodoList todos={todos} />
     </TodoTemplate>
   );
